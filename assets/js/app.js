ed528413d95e88290fd008efdd9c65e3e8a6087e
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-05-26 16:01:26 -0600

    app returns random place to eat

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+
+// empty array that will hold the restaurants
+
+var whereToEat = []
+
+// get value of form from submit button
+$("#submit-button").on("click", function() {
+    // get the value
+    var userInput = $("#places-input").val().trim(); 
+
+    // split the user Input from a string to single restaurants
+    whereToEat = whereToEat.concat(userInput.split(", "));
+
+    // call the function that returns the random restaurants
+    randomize();
+})
+
+function randomize() {
+
+    var randomPlace = Math.floor(Math.random() * whereToEat.length);
+    var randomResponse = whereToEat[randomPlace];
+    console.log(randomResponse)
+}
