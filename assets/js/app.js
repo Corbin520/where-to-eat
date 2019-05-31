f8fd58f06a41d922eba1be7113c67ef742697758
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-05-30 20:31:08 -0600

    working on enter press to run function for mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,58 +4,60 @@ var finalLocation = document.getElementById("random-option");
 var whereToEat = []
 var randomResponse
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7608, lng: -111.8910},
-    zoom: 9
-  });
-}
 
 
 $("#submit-button").on("click", function() {
     
     var userInput = $("#places-input").val().trim(); 
-
+    
     whereToEat = whereToEat.concat(userInput.split(", "));
-
+    
     randomize();
-
+    
     $("#places-input").hide();
     $("#submit-button").hide();
-
+    
     $("#random-option").show();
     $("#where-you-are-eating").show();
 })
 
-// try below method.
+// try below method. on press
 // on press
 
-// $("#places-input").on("keyup", function(event) {
-//     event.preventDefault()
+$("#places-input").on("keypress", function(event) {
+        event.preventDefault()
     
-//     var userInput = $("#places-input").val().trim(); 
+        var userInput = $("#places-input").val().trim(); 
+    
+        whereToEat = whereToEat.concat(userInput.split(", "));
+    
+        randomize();
+    
+        $("#places-input").hide();
+        $("#submit-button").hide();
+    
+        $("#random-option").show();
+        $("#where-you-are-eating").show();
+    })
+    
 
-//     whereToEat = whereToEat.concat(userInput.split(", "));
+    // function that takes in our users input and gives back a random 
+    function randomize() {
+        
+        var randomPlace = Math.floor(Math.random() * whereToEat.length);
+        randomResponse = whereToEat[randomPlace];
+        finalLocation.textContent = randomResponse;
+    }
+    
+    // Google Maps
+    var map;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.7608, lng: -111.8910},
+        zoom: 9
+      });
+    }
 
-//     randomize();
-
-//     $("#places-input").hide();
-//     $("#submit-button").hide();
-
-//     $("#random-option").show();
-//     $("#where-you-are-eating").show();
-// })
-
-function randomize() {
-
-    var randomPlace = Math.floor(Math.random() * whereToEat.length);
-    randomResponse = whereToEat[randomPlace];
-    finalLocation.textContent = randomResponse;
-}
-
-
-// bring in google maps so we can drop a pin where the locations are of the user guess
 
 
 
