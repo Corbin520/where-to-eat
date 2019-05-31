bd7639b9ce4fc6f7a2e96995b1f2e997e8d05c20
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-05-31 14:37:23 -0600

    working on location of device

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 The point of this application is to help anyone who is trying to decide on a place
 to eat. It will take in multiple places to eat, and will spit out a random option.
+
+I am now beginning to work on it making it have google maps API so the user can see
+places near them as well as get their location upon going to the web page to better
+assist with locations.
--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ var whereToEat = []
 var randomResponse
 
 
-
+// when submit button is hit, run the code
 $("#submit-button").on("click", function() {
     
     var userInput = $("#places-input").val().trim(); 
@@ -20,6 +20,50 @@ $("#submit-button").on("click", function() {
     $("#random-option").show();
     $("#where-you-are-eating").show();
 })
+
+
+// function that takes in our users input and gives back a random 
+function randomize() {
+    
+    var randomPlace = Math.floor(Math.random() * whereToEat.length);
+    randomResponse = whereToEat[randomPlace];
+    finalLocation.textContent = randomResponse;
+}
+
+
+// Google Maps
+// https://developers.google.com/maps/documentation/javascript/geolocation
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.7608, lng: -111.8910},
+        zoom: 9
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+
+
 
 // try below method. on press
 // on press
@@ -39,28 +83,6 @@ $("#submit-button").on("click", function() {
     //     $("#random-option").show();
     //     $("#where-you-are-eating").show();
     // })
-    
-
-    // function that takes in our users input and gives back a random 
-    function randomize() {
-        
-        var randomPlace = Math.floor(Math.random() * whereToEat.length);
-        randomResponse = whereToEat[randomPlace];
-        finalLocation.textContent = randomResponse;
-    }
-    
-    // Google Maps
-    var map;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.7608, lng: -111.8910},
-        zoom: 9
-      });
-    }
-
-
-
-
 
 
 
